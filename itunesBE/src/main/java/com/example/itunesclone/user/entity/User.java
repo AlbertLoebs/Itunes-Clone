83d7e7c22528e7560f0d553b4b2733eda7560bd5
Albert Loebs <albertloebs1@gmail.com>
2026-01-14 15:17:50 -0800

    add persisted H2 database and User entity

--- itunesBE/src/main/java/com/example/itunesclone/user/entity/User.java
+++ itunesBE/src/main/java/com/example/itunesclone/user/entity/User.java
@@ -1,4 +1,78 @@
 package com.example.itunesclone.user.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String role;
+
+    public User() {}
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPasswordHash(){
+        return passwordHash;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash){
+        this.passwordHash = passwordHash;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
 }
